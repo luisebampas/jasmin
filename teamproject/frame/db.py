d0060a21a20ce0ea4e3db52929ac8f971b29d7d0
soomin98 <sm7237@naver.com>
2021-02-08 15:24:38 +0900

    0208plusmap

--- teamproject/frame/db.py
+++ teamproject/frame/db.py
@@ -1,7 +1,6 @@
 import MySQLdb
 
 config = {
-<<<<<<< HEAD
     'database':'jasmine',
     'user':'root',
     'password':'111111',
@@ -21,25 +20,3 @@ class Db:
             cursor.close();
         if conn != None:
             cursor.close();
-=======
-    'database': 'jasmindb',
-    'user': 'root',
-    'password': '111111',
-    'host': '127.0.0.1',
-    'port': 3306,
-    'charset': 'utf8',
-    'use_unicode': True
-}
-
-
-class Db:
-    def getConnection(self):
-        conn = MySQLdb.connect(**config)
-        return conn
-
-    def close(self, conn, cursor):
-        if cursor != None:
-            cursor.close()
-        if conn != None:
-            cursor.close()
->>>>>>> aad851d185dadcdc087b7f21a795bba5c1a79d51
--- teamproject/frame/value.py
+++ teamproject/frame/value.py
@@ -1,5 +1,4 @@
 class User:
-<<<<<<< HEAD
     def __init__(self, usernum, userid, userpwd, username):
         self.usernum = usernum;
         self.userid = userid;
@@ -9,25 +8,22 @@ class User:
     def __str__(self):
         return str(self.usernum)+' '+self.userid+' '+self.userpwd+' '+self.username+' ';
 
-=======
-    def __init__(self, id, pwd, name):
-        self.id = id;
-        self.pwd = pwd;
-        self.name = name;
+class Orderlist:
+    def __init__(self,  ordernum, usernum, itemnum ):
+        self.ordernum = ordernum;
+        self.usernum = usernum;
+        self.itemnum = itemnum;
 
     def __str__(self):
-        return self.id + ' ' + self.pwd + ' ' + self.name + ' ';
+        return str(self.ordernum)+' '+str(self.usernum)+' '+str(self.itemnum)+' ';
 
-
-class Item:
-    def __init__(self, id, name, price, regdate, imgname):
-        self.id = id;
-        self.name = name;
+class Mainlist:
+    def __init__(self, ordernum, itemnum, itemname, price, authorname ):
+        self.ordernum = ordernum;
+        self.itemnum = itemnum;
+        self.itemname = itemname;
         self.price = price;
-        self.regdate = regdate;
-        self.imgname = imgname;
-
+        self.authorname = authorname;
     def __str__(self):
-        return str(self.id) + ' ' + self.name + ' ' \
-               + str(self.price) + ' ' + str(self.regdate) + ' ' + self.imgname;
->>>>>>> aad851d185dadcdc087b7f21a795bba5c1a79d51
+        return str(self.ordernum)+' '+str(self.itemnum)+' '+self.itemname+' '+str(self.price)+' '+self.authorname+' ';
+

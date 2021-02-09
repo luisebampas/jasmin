ee9bdb0cdaa98db895f4df9a79ec7b248c7b3bca
soomin98 <sm7237@naver.com>
2021-02-09 17:08:28 +0900

    0209final

---
+++ teamprojectsoomin/frame/value.py
@@ -0,0 +1,69 @@
+
+
+class User:
+    def __init__(self, usernum, userid, userpwd, username):
+        self.usernum = usernum;
+        self.userid = userid;
+        self.userpwd = userpwd;
+        self.username = username;
+
+    def __str__(self):
+        return str(self.usernum)+' '+self.userid+' '+self.userpwd+' '+self.username+' ';
+
+class Orderlist:
+    def __init__(self,  ordernum, usernum, itemnum ):
+        self.ordernum = ordernum;
+        self.usernum = usernum;
+        self.itemnum = itemnum;
+
+    def __str__(self):
+        return str(self.ordernum)+' '+str(self.usernum)+' '+str(self.itemnum)+' ';
+
+class Mainlist:
+    def __init__(self, ordernum, itemnum, itemname, price, authorname ):
+        self.ordernum = ordernum;
+        self.itemnum = itemnum;
+        self.itemname = itemname;
+        self.price = price;
+        self.authorname = authorname;
+    def __str__(self):
+        return str(self.ordernum)+' '+str(self.itemnum)+' '+self.itemname+' '+str(self.price)+' '+self.authorname+' ';
+
+class Cartlist:
+    def __init__(self,cartnum, itemnum, itemname, price):
+        self.cartnum = cartnum;
+        self.itemnum = itemnum;
+        self.itemname = itemname;
+        self.price = price;
+    def __str__(self):
+        return str(self.cartnum)+' '+str(self.itemnum)+' '+self.itemname+' '+str(self.price)+' ';
+
+class Item:
+    def __init__(self, itemnum, catenum, authornum, itemname, price, itemdate, iteminfo, sells, series):
+        self.itemnum = itemnum;
+        self.catenum = catenum;
+        self.authornum = authornum;
+        self.itemname = itemname;
+        self.price = price;
+        self.itemdate = itemdate;
+        self.iteminfo = iteminfo;
+        self.sells = sells;
+        self.series = series;
+
+    def __str__(self):
+        return str(self.itemnum) + ' ' + str(self.catenum) + ' ' + str(self.authornum) + ' ' + \
+               self.itemname + ' ' + str(self.price) + ' ' + str(self.itemdate) + ' ' + \
+               self.iteminfo + ' ' + str(self.sells) + ' ' + str(self.series);
+
+
+class Itemlist:
+    def __init__(self, itemnum, itemname, authorname, price, itemdate):
+        self.itemnum = itemnum;
+        self.itemname = itemname;
+        self.authorname = authorname;
+        self.price = price;
+        self.itemdate = itemdate;
+
+    def __str__(self):
+        return str(self.itemnum) + ' ' + self.itemname + ' ' + self.authorname + ' ' + \
+               str(self.price) + ' ' + str(self.itemdate);
---
+++ teamprojectsoomin/jasmine/urls.py
@@ -0,0 +1,54 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+from jasmine import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name='jasmine/home.html'), name='home'),
+    path('home',TemplateView.as_view(template_name='jasmine/home.html'), name='home'),
+
+    path('login', views.MainView.login, name='login'),
+    path('join', views.MainView.join, name='join'),
+    path('joinimpl', views.MainView.joinimpl, name='joinimpl'),
+    path('loginimpl', views.MainView.loginimpl, name='loginimpl'),
+    path('logout', views.MainView.logout, name='logout'),
+    path('mypage', views.mypage, name='mypage'),
+    path('userdetail', views.userdetail, name='userdetail'),
+    path('userupdate', views.userupdate, name='userupdate'),
+    path('userupdateimpl', views.userupdateimpl, name='userupdateimpl'),
+    path('userdelete', views.userdelete, name='userdelete'),
+    path('orderlist', views.orderlist, name='orderlist'),
+    path('cart', views.cart, name='cart'),
+    path('cartdelete', views.cartdelete, name='cartdelete'),
+
+    path('mainSection', views.mainSectionView.mainSection, name='mainSection'),
+    path('itemlist', views.mainSectionView.itemlist, name='itemlist'),
+    path('itemcontent', views.mainSectionView.itemcontent, name='itemcontent'),
+    path('payment', views.mainSectionView.payment, name='payment'),
+    path('paydetail', views.mainSectionView.paydetail, name='paydetail'),
+
+    path('sideSection', views.sideSectionView.sideSection, name='sideSection'),
+    path('category', views.sideSectionView.category, name='category'),
+
+    path('about', TemplateView.as_view(template_name='jasmine/about.html'), name='about'),
+    path('map', views.map, name='map'),
+
+]
